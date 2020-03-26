cb263f78481cd4f402d4fdf3c0c9f11a8c2c0640
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-26 16:57:12 -0700

    FINALLY target correct inputs; add further logic to remove previous results if multiple submits

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,34 @@
 $(document).ready(function() {
-  // var height = parseInt(prompt("What is your height in feet?"));
-
-  // if (height >= 7) {
-  //   $("#over7").show();
-  // } else if (height >= 5) {
-  //   $("#over5under7").show();
-  // } else if (height < 5) {
-  //   $("#under5").show();
-  // } else {
-  //   console.log("Unexpected input?")
-  // }
-
-  $("#submit").submit(function() {
+  
+  $("form").submit(function(event) {
     event.preventDefault();
-    var height = $("select#height").val();
+    var height = $("#height").val();
     console.log(height);
-    
-    // if (height === "#under5") {
-    //   $("#under5").show();
-    // } else if (height === "#over5under7") {
-    //   $("#over5under7").show();
-    // } else if (height === "#over7") {
-    //   $("#over7").show();
-    // }
+
+    if (height === "under5") {
+      $("#over5under7").hide();
+      $("#over5under7").hide();
+      $("#under5").show();
+    } else if (height === "over5under7") {
+      $("#under5").hide();
+      $("#over7").hide();
+      $("#over5under7").show();
+    } else if (height === "over7") {
+      $("#under5").hide(); 
+      $("#over5under7").hide();
+      $("#over7").show();
+    };
   });
 });
+
+// var height = parseInt(prompt("What is your height in feet?"));
+
+// if (height >= 7) {
+//   $("#over7").show();
+// } else if (height >= 5) {
+//   $("#over5under7").show();
+// } else if (height < 5) {
+//   $("#under5").show();
+// } else {
+//   console.log("Unexpected input?")
+// }

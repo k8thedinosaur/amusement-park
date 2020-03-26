e47e926cc3e8e87d4c2cae2a59a6c49cdb6a6455
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-26 12:06:00 -0700

    converts to form input and breaks everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,30 @@
-// $(document).ready(function() {
-//   var age = parseInt(prompt("How old are you?"));
- 
-//   if (age > 21) {
-//     $("#drinks").show();
-//   } else if (age === 21) {
-//     alert("Happy birthyear! Go crazy!");
-//     $("#drinks").show();
-//   } else {
-//     $("#under-21").show();
-//   }
-// });
-
 $(document).ready(function() {
-  var height = parseInt(prompt("What is your height in feet?"));
-
-  if (height >= 7) {
-    $("#over7").show();
-  } else if (height >= 5) {
-    $("#over5under7").show();
-  } else if (height < 5) {
-    $("#under5").show();
-  } else {
-    console.log("Unexpected input?")
-  }
-})
+  // var height = parseInt(prompt("What is your height in feet?"));
+  
+  // if (height >= 7) {
+    //   $("#over7").show();
+    // } else if (height >= 5) {
+      //   $("#over5under7").show();
+      // } else if (height < 5) {
+        //   $("#under5").show();
+        // } else {
+          //   console.log("Unexpected input?")
+          // }
+          
+    $("#height form").submit(function() {
+    
+    
+    var height = $("#height").val();
+    console.log(height);
+    
+      if (height === "#under5") {
+        $("#under5").show();
+      } else if (height === "#over5under7") {
+        $("#over5under7").show();
+      } else if (height === "#over7") {
+        $("#over7").show();
+      }
+  
+  event.preventDefault();
+  }) 
+});

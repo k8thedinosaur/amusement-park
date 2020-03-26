7211aa281ec1ac8f66b275f24bc7005810bddb28
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-26 13:44:57 -0700

    everything be more broken than ever before. moving on.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,27 @@
 $(document).ready(function() {
   // var height = parseInt(prompt("What is your height in feet?"));
-  
+
   // if (height >= 7) {
-    //   $("#over7").show();
-    // } else if (height >= 5) {
-      //   $("#over5under7").show();
-      // } else if (height < 5) {
-        //   $("#under5").show();
-        // } else {
-          //   console.log("Unexpected input?")
-          // }
-          
-    $("#height form").submit(function() {
-    
-    
-    var height = $("#height").val();
+  //   $("#over7").show();
+  // } else if (height >= 5) {
+  //   $("#over5under7").show();
+  // } else if (height < 5) {
+  //   $("#under5").show();
+  // } else {
+  //   console.log("Unexpected input?")
+  // }
+
+  $("#submit").submit(function() {
+    event.preventDefault();
+    var height = $("select#height").val();
     console.log(height);
     
-      if (height === "#under5") {
-        $("#under5").show();
-      } else if (height === "#over5under7") {
-        $("#over5under7").show();
-      } else if (height === "#over7") {
-        $("#over7").show();
-      }
-  
-  event.preventDefault();
-  }) 
+    // if (height === "#under5") {
+    //   $("#under5").show();
+    // } else if (height === "#over5under7") {
+    //   $("#over5under7").show();
+    // } else if (height === "#over7") {
+    //   $("#over7").show();
+    // }
+  });
 });
